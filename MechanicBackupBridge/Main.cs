e2d1ed6a9419dc1f1de1fa8f763056b9b2a12c62
craftycram <github@craftycram.net>
2020-09-19 18:02:26 +0200

    adding rage domain support

--- MechanicBackupBridge/Main.cs
+++ MechanicBackupBridge/Main.cs
@@ -1,32 +1,55 @@
-﻿using System:
+﻿using System;
 using Rage;
 using LSPD_First_Response.Mod.API;
+using System.Reflection;
 
 namespace MechanicBackupBridge
 {
     public class Main : Plugin
     {
-        public override void Finally()
+        public override void Initialize()
         {
 
             Version currentVersion = new Version("1.0.0");
             Version newVersion = new Version();
-
-            Functions.OnOnDutyStateChanged += OnDutyStateChangedHandler;
  
             Game.DisplayNotification("MechanicBackupBridge " + currentVersion + " loaded successfully");
             Game.LogTrivial("MechanicBackupBridge " + currentVersion + " loaded successfully");
 
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            AppDomain rageDomain = AppDomain.CreateDomain("rageDomain");
+
+            currentDomain.AssemblyResolve += new ResolveEventHandler(MyResolveEventHandler);
+
+            // This call will succeed in creating an instance of MyType since the
+            // assembly name is valid.
+            InstantiateMyTypeSucceed(currentDomain);
+
         }
 
-        private static void OnDutyStateChangedHandler(bool OnDuty)
-        {
-
-        }
-
-        public override void Initialize()
+        public override void Finally()
         {
             Game.LogTrivial("MechanicBackupBridge unloaded");
+        }
+
+        private static void InstantiateMyTypeSucceed(AppDomain domain)
+        {
+            try
+            {
+                string asmname = Assembly.GetCallingAssembly().FullName;
+                domain.CreateInstance(asmname, "MechanicBackup.API");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static Assembly MyResolveEventHandler(object sender, ResolveEventArgs args)
+        {
+            Console.WriteLine("Resolving...");
+            return typeof(MechanicBackup.API).Assembly;
         }
     }
 }
