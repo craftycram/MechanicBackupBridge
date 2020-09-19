085ea2f18624063157ee033c2bc6dd183ed6c938
craftycram <github@craftycram.net>
2020-09-19 02:52:08 +0200

    initial commit

---
+++ MechanicBackupBridge/API.cs
@@ -0,0 +1,25 @@
+﻿
+namespace MechanicBackupBridge
+{
+    public static class API
+    {
+
+        public static void spawnMechanicUnit()
+        {
+            MechanicBackup.API.spawnMechanicUnit();
+        }
+        public static void spawnTowingUnit()
+        {
+            MechanicBackup.API.spawnTowingUnit();
+        }
+        public static void spawnPickupUnit()
+        {
+            MechanicBackup.API.spawnPickupUnit();
+        }
+        public static void spawnDutyVehicleUnit()
+        {
+            MechanicBackup.API.spawnDutyVehicleUnit();
+        }
+
+    }
+}
---
+++ MechanicBackupBridge/Main.cs
@@ -0,0 +1,32 @@
+﻿using System:
+using Rage;
+using LSPD_First_Response.Mod.API;
+
+namespace MechanicBackupBridge
+{
+    public class Main : Plugin
+    {
+        public override void Finally()
+        {
+
+            Version currentVersion = new Version("1.0.0");
+            Version newVersion = new Version();
+
+            Functions.OnOnDutyStateChanged += OnDutyStateChangedHandler;
+ 
+            Game.DisplayNotification("MechanicBackupBridge " + currentVersion + " loaded successfully");
+            Game.LogTrivial("MechanicBackupBridge " + currentVersion + " loaded successfully");
+
+        }
+
+        private static void OnDutyStateChangedHandler(bool OnDuty)
+        {
+
+        }
+
+        public override void Initialize()
+        {
+            Game.LogTrivial("MechanicBackupBridge unloaded");
+        }
+    }
+}
