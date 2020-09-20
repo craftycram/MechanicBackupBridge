a7f9a5341ad1273a5570c5e97840abd66a694dd7
craftycram <github@craftycram.net>
2020-09-20 04:04:08 +0200

    added update checker

--- MechanicBackupBridge/Main.cs
+++ MechanicBackupBridge/Main.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Net;
 using Rage;
 using LSPD_First_Response.Mod.API;
 using System.Reflection;
@@ -11,13 +13,51 @@ namespace MechanicBackupBridge
         public override void Initialize()
         {
 
-            Version currentVersion = new Version("1.0.0");
+            Version currentVersion = new Version("1.1.0");
             Version newVersion = new Version();
  
             Game.DisplayNotification("MechanicBackupBridge " + currentVersion + " loaded successfully");
             Game.LogTrivial("MechanicBackupBridge " + currentVersion + " loaded successfully");
 
             AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
+
+            try
+            {
+                Thread FetchVersionThread = new Thread(() =>
+                {
+
+                    using (WebClient client = new WebClient())
+                    {
+                        try
+                        {
+                            string s = client.DownloadString("http://www.lcpdfr.com/applications/downloadsng/interface/api.php?do=checkForUpdates&fileId=29806&textOnly=1");
+
+                            newVersion = new Version(s);
+                        }
+                        catch (Exception e) { Game.LogTrivial("MechanicBackupBridge: LSPDFR Update API down. Aborting checks."); }
+                    }
+                });
+                FetchVersionThread.Start();
+                while (FetchVersionThread.ThreadState != System.Threading.ThreadState.Stopped)
+                {
+                    GameFiber.Yield();
+                }
+
+                // compare the versions  
+                if (currentVersion.CompareTo(newVersion) < 0)
+                {
+                    Game.LogTrivial("MechanicBackupBridge: Update Available for MechanicBackupBridge. Installed Version " + currentVersion + "New Version " + newVersion);
+                    Game.DisplayNotification("~g~Update Available~w~ for ~b~MechanicBackupBridge~w~.\nInstalled Version: ~y~" + currentVersion + "\n~w~New Version~y~ " + newVersion);
+                }
+            }
+            catch (System.Threading.ThreadAbortException e)
+            {
+                Game.LogTrivial("MechanicBackupBridge: Error while checking for updates.");
+            }
+            catch (Exception e)
+            {
+                Game.LogTrivial("MechanicBackupBridge: Error while checking for updates.");
+            }
 
         }
 
